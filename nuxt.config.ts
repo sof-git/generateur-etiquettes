a6dedb19519ab188f2b53f5dda16eb06484eeaf2
sofiane-turki <sofiane.turki01@gmail.com>
2026-08-27 13:54:32 +0200

    Fix GitHub Pages base URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+    baseURL: '/generateur-etiquettes/'
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['vuetify-nuxt-module']
