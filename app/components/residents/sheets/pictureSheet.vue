d30edbf6aa9efa7fe07292224043868b97966c25
sofiane-turki <sofiane.turki01@gmail.com>
2026-09-23 09:56:32 +0200

    added pictureSheet

--- app/components/residents/sheets/pictureSheet.vue
+++ app/components/residents/sheets/pictureSheet.vue
@@ -1,9 +1,38 @@
 <template>
-    <v-sheet class="sheet">
+    <v-sheet class="sheet" color="white" elevation="3" rounded="lg">
         <v-container>
             <v-row>
-                <v-col>
-                    
+                <v-col
+                v-for="index in 12"
+                :key="index"
+                cols="2"
+                class="pictures-row"
+                >
+                    <v-img
+                        class="border-lg border-blackBorder"
+                        :src="pictureTemplate.src"
+                        width="100%"
+                    />
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col cols="6">
+                    <v-img
+                        class="border-lg border-blackBorder"
+                        :src="pictureTemplate.src"
+                        width="100%"
+                    />                    
+                </v-col>
+                <v-col offset="2" cols="4">
+                    <v-img
+                    v-for="index in 2"
+                    :key="index"
+                    class="border-lg border-blackBorder ma-1"
+                    :src="pictureTemplate.src"
+                    width="100%"
+                    aspect-ratio="1"
+                    cover
+                    />
                 </v-col>
             </v-row>
         </v-container>
@@ -20,4 +49,5 @@ const { pictureTemplate } = useResident();
   width: 210mm;
   height: 297mm;
 }
+
 </style>
